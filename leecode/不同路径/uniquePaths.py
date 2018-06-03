2d42b2b80b0f8d40e89847b08590cfb2a34e56a7
JCP@310 <jcp0578@gmail.com>
2018-06-03 20:03:34 +0800

    uniquePaths - AC

--- leecode/不同路径/uniquePaths.py
+++ leecode/不同路径/uniquePaths.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 '''
-time out
+AC
 '''
 
 import time
@@ -14,24 +14,22 @@ class Solution:
         :type n: int
         :rtype: int
         """
-        if m > 0 and n > 0:
-            if m == 1:
-                return 1
-            elif n == 1:
-                return 1
-            else:
-                return self.uniquePaths(m, n - 1) + self.uniquePaths(m - 1, n)
-        else:
+        if m < 1 or n < 1:  
             return 0
+        record = [[1]*n for j in range(m)]  
+        for i in range(1, m):  
+            for j in range(1, n):  
+                record[i][j] = record[i - 1][j] + record[i][j - 1]  
+        return record[m - 1][n - 1]  
 
 
 if __name__ == "__main__":
 
     t0 = time.perf_counter()
 
-    test_list = [3, 7, 2]
-    test_list_2 = [2, 3, 2]
-    answer_list = [3, 28, 2]
+    test_list = [3, 7, 23]
+    test_list_2 = [2, 3, 12]
+    answer_list = [3, 28, 193536720]
 
     test = Solution()
     for i in range(len(test_list)):
@@ -46,7 +44,50 @@ if __name__ == "__main__":
                    str(test_list[i]) + " " + str(test_list_2[i])))
 
     print("\nRun Time is %f s" % (time.perf_counter() - t0))
-    
+
 '''
 
+class Solution:
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        超时C(m + n - 2, n - 1)：
+        if m < n :
+            m,n = n,m
+        combins = [c for c in  itertools.combinations(range(m+n-2), n-1)]
+        out = len(combins)
+        return out
+        
+        from functools import reduce
+        if m < n:
+            m, n = n, m
+        mul = lambda x, y: reduce(operator.mul, range(x, y), 1)
+        return int(mul(m, m + n - 1) / mul(1, n))
+        """
+        v = [[1] * n for x in range(m) ]
+        for i in range(m):
+            for j in range(n):
+                if i >= 1 and j >= 1:
+                    v[i][j] = v[i][j-1]+v[i-1][j]
+        return v[m-1][n-1]
+
+
+class Solution:
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        dp = [0 for col in range(n)]
+        for i in range(n):
+            dp[i] = 1
+            
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[j] = dp[j-1] + dp[j]
+        
+        return dp[n-1]
 '''
