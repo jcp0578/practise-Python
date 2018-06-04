b259f562384f4502de20955878b9228cf0baa13d
JCP@310 <jcp0578@gmail.com>
2018-06-04 14:26:16 +0800

    myPow -AC loop

--- leecode/Pow(x, n)/myPow.py
+++ leecode/Pow(x, n)/myPow.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 '''
-AC 
+AC -loop
 '''
 
 import time
+
 #from math import round
+
 
 class Solution:
     def myPow(self, x, n):
@@ -14,51 +16,46 @@ class Solution:
         :type n: int
         :rtype: float
         """
-
-        if n ==0:
+        flag = 0
+        out_t = 1
+        if n == 0:
             return 1
-        elif n<0:
-            return 1/self.myPow(x,-n)
-        elif n==2:
-            return x*x
+        elif n < 0:
+            flag = 1
+            n = -n
 
-        if x == 0:
-            return 0.0
-        elif x==1:
-            return 1.0
-        temp= self.myPow(x,n//2)
-        if n%2:
-            return x*self.myPow(temp,2)
+        while n:
+            if n & 1:
+                out_t *= x
+            n //= 2
+            x *= x
+        if flag:
+            return 1 / out_t
         else:
-            return self.myPow(temp,2)
-
-
-        
-
+            return out_t
 
 
 if __name__ == "__main__":
 
     t0 = time.perf_counter()
 
-    test_list = [2.00000,2.10000,2.00000,2.15000,0.999]
-    test_list_2=[10,3,-2,10,2147483647]
-    answer_list = [1024.00000,9.26100,0.25,2110.49632,0.0]
+    test_list = [2.00000, 2.10000, 2.00000, 2.15000, 0.999]
+    test_list_2 = [10, 3, -2, 10, 2147483647]
+    answer_list = [1024.00000, 9.26100, 0.25, 2110.49632, 0.0]
 
     test = Solution()
     for i in range(len(test_list)):
-        out_t = test.myPow(test_list[i],test_list_2[i])
+        out_t = test.myPow(test_list[i], test_list_2[i])
 
         if out_t == answer_list[i]:
             print("\033[1;32;40m  Pass \033[0m")
         else:
             print(
                 "\033[1;31;40m  Fail!!\033[0m\033[0;34;40m out \"%s\" should \"%s\" by \"%.50s\" "
-                % (out_t, answer_list[i],str(test_list[i])+"   "+str(test_list_2[i])))
+                % (out_t, answer_list[i],
+                   str(test_list[i]) + "   " + str(test_list_2[i])))
 
     print("\nRun Time is %f s" % (time.perf_counter() - t0))
-
-
 '''
 class Solution:
     def myPow(self, x, n):
