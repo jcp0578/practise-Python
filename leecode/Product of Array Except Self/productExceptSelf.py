1503ecd1f6f13f4b34407549507f106c36f2c06e
JCP@310 <jcp0578@gmail.com>
2018-06-05 22:15:12 +0800

    productExceptSelf - AC

--- leecode/Product of Array Except Self/productExceptSelf.py
+++ leecode/Product of Array Except Self/productExceptSelf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 '''
-AC but not a good solution
+AC
 '''
 import sys
 import time
@@ -13,16 +13,17 @@ class Solution:
         :type nums: List[int]
         :rtype: List[int]
         """
-        front_nums = [1]
+        if not nums:
+            return []
+        out_t = [1]
         len_t = len(nums)
         for i in range(1, len_t):
-            front_nums.append(front_nums[-1] * nums[i - 1])
-        reverse_nums = nums[::-1]
-        back_nums = [1]
-        for i in range(1, len_t):
-            back_nums.append(back_nums[-1] * reverse_nums[i - 1])
-        back_nums = back_nums[::-1]
-        return [front_nums[i] * back_nums[i] for i in range(len_t)]
+            out_t.append(out_t[i - 1] * nums[i - 1])
+        back_product=1
+        for i in range(len_t-1,-1,-1):
+            out_t[i]*=back_product
+            back_product*=nums[i]
+        return out_t
 
 
 if __name__ == "__main__":
@@ -32,8 +33,8 @@ if __name__ == "__main__":
     else:
         t0 = time.time()
 
-    test_list = [[1, 2, 3, 4], []]
-    answer_list = [[24, 12, 8, 6], []]
+    test_list = [[1, 2, 3, 4], [2]]
+    answer_list = [[24, 12, 8, 6], [1]]
 
     test = Solution()
     for i in range(len(test_list)):
@@ -51,5 +52,40 @@ if __name__ == "__main__":
     else:
         print("\nRun Time is %f s" % (time.time() - t0))
 '''
+class Solution:
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        p = 1
+        n = len(nums)
+        output = []
+        for i in range(0,n):
+            output.append(p)
+            p = p * nums[i]
+        p = 1
+        for i in range(n-1,-1,-1):
+            output[i] = output[i] * p
+            p = p * nums[i]
+        # It calculates the left and right part of the product independently.
+        return output
 
+
+class Solution:
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        if not nums: return False
+        
+        arr = [1] * len(nums)
+        pi = pj = 1
+
+        for i in range(len(nums)):
+            j = -1-i
+
+            arr[i]*=pi; arr[j]*=pj
+            pi *= nums[i]; pj *= nums[j]  
 '''
