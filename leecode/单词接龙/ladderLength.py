16f59fa76c008c825d581fbc53a3ee4e6575015b
JCP@310 <jcp0578@gmail.com>
2018-06-11 23:16:15 +0800

    ladderLength -time out 29/39

--- leecode/单词接龙/ladderLength.py
+++ leecode/单词接龙/ladderLength.py
@@ -1,80 +1,31 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 '''
-time out 21/39
+time out 29/39
 '''
 import sys
 import time
 
-
+import collections
+import string
 class Solution:
-    def wordCompare(self, word1, word2):
-        """
-        :type word1: str
-        :type word2: str
-        len(word1)=len(word2)
-        :rtype: bool
-        """
-        diff = 0
-        for i in range(len(word1)):
-            if word1[i] != word2[i]:
-                diff += 1
-                if diff > 1:
-                    return False
-        return True
-
     def ladderLength(self, beginWord, endWord, wordList):
-        """
-        :type beginWord: str
-        :type endWord: str
-        :type wordList: List[str]
-        :rtype: int
-        """
-        if not beginWord:
-            return 0
-        if endWord not in wordList:
-            return 0
-        wordList.append(beginWord)
-        wordDice = {}
-        for word in wordList:
-            wordDice[word] = []
-        len_t = len(wordList)
-        min_t = len_t + 2
-        min_word_depth=dict()
-        for i in range(len_t):
-            for j in range(i):
-                if self.wordCompare(wordList[i], wordList[j]):
-                    wordDice[wordList[i]].append(wordList[j])
-                    wordDice[wordList[j]].append(wordList[i])
-
-        def findWord(wordlist_t, depth):
-            """
-            :type word1: str
-            :type word2: str
-            len(word1)=len(word2)
-            :rtype: bool
-            """
-            nonlocal min_t
-            nonlocal min_word_depth
-            if wordlist_t[-1] == endWord:
-                if depth < min_t:
-                    min_t = depth
-                    return 1
-                if depth < (min_word_depth[wordlist_t[-1]]):
-                    min_word_depth[wordlist_t[-1]]=depth
-                else:
-                    return 0
-            except KeyError:
-                min_word_depth[wordlist_t[-1]]=depth
-
-            for word_t in wordDice[wordlist_t[-1]]:
-                    findWord(wordlist_t, depth + 1)
-                    wordlist_t.pop()
-            return 0
-
-        findWord([beginWord], 1)
-
-        return min_t if min_t < len_t + 2 else 0
+        # wordList.add(endWord)
+        queue = collections.deque([(beginWord, 1)])
+        ls = string.ascii_lowercase
+        visited = set()
+        while queue:
+            word, dist = queue.popleft()
+            if word == endWord:
+                return dist
+            for i in range(len(word)):
+                for j in ls:
+                    if j != word[i]:
+                        newWord = word[:i]+j+word[i+1:]
+                        if newWord not in visited and newWord in wordList:
+                            queue.append((newWord, dist+1))
+                            visited.add(newWord)  # wordList.remove(newWord)
+        return 0
 
 
 if __name__ == "__main__":
